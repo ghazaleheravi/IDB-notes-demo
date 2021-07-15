a1a6d3639ba9057bb1ba632abd4dd0ab2cd8c97c
Ghazaleh Heravi <ghazaleheravi@users.noreply.github.com>
2021-07-15 12:33:33 -0400

    setting up DB

--- index.js
+++ index.js
@@ -13,6 +13,7 @@ const btn = document.querySelector('button');
 let db;
 
 window.onload = function() {
+
   //request opening a connetion to a database, open version 1 of a database called notes_db
   //then we need handlers for our opening result
   let request = window.indexedDB.open('notes_db', 1);
@@ -23,12 +24,29 @@ window.onload = function() {
 
   request.onsuccess = function() {
     console.log('Database opened successfully');
+
+    //IDBRequest.result- returns the result of the request
     db = request.result;
     displayData();
   }
 
+  //run if the database has not already been set up,or if the database is opened with a bigger version
   request.onupgradeneeded = function(e) {
     db = e.target.result;
+
+    //create an objectStore to store our notes, store note_os inside db(our opened database)
+    //objectSotre is basically like a single table
+    //we can use keyPath property to uniquely identify individual records in the store, such as deleting or displaying a record
+    let objectStore = db.createObjectStore('notes_os', {keyPath: 'id', autoIncreament:true});
+    
+    //IDBObjectStore.createIndex()- return a new IDBIndex object in the connected database
+    //simple words: creates a new field/column(index) defining a new data point for each database record to contain
+    //An index is kind of objectStore for looking up records in another objectStore
+    objectStore.createIndex('title', 'title', {unique: false});
+    objectStore.createIndex('body', 'body', {unique: false});
+
+    console.log('Data setup complete');
+
 
   }
 };
